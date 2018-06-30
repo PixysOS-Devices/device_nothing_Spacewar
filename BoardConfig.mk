feec24ef1f4cdd2bc027bae6ba29a66bc41e6bc4
Bruno Martins <bgcngm@gmail.com>
2018-06-30 10:49:31 +0100

    Spacewar: Import BT config header
     * Straight from CAF (as per LA.UM.6.3.r4-04300-sdm845.0),
       with BTM_DEF_LOCAL_NAME unset so that ro.product.model
       is used instead.
    
    Change-Id: I20a8460c9d936cc3d94f74e469f8babf1c02bda4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -70,6 +70,9 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_SOURCE := kernel/nothing/sm7325
 TARGET_KERNEL_CONFIG := vendor/lahaina-qgki_defconfig
 
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
+
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
 
