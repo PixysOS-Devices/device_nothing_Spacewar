4311838100918eeee741ec7339b8cda82678ab05
Alexander Koskovich <akoskovich@pm.me>
2022-04-26 21:18:08 -0700

    Spacewar: Remove virtual framebuffer
     * It's largely for testing purposes and it consumes
       a large amount of memory. If your hardware doesn't
       have graphics then it can be useful, which isn't the
       case for shipped devices.
    
    Change-Id: I2348718ad3afba846498572161fa7e83dd8b7ce9
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,7 +53,6 @@ BOARD_KERNEL_CMDLINE := \
     androidboot.hardware=qcom \
     androidboot.memcg=1 \
     lpm_levels.sleep_disabled=1 \
-    video=vfb:640x400,bpp=32,memsize=3072000 \
     msm_rtb.filter=0x237 \
     service_locator.enable=1 \
     androidboot.usbcontroller=a600000.dwc3 \
@@ -64,7 +63,6 @@ BOARD_KERNEL_CMDLINE := \
     loop.max_part=7 \
     iptable_raw.raw_before_defrag=1 \
     ip6table_raw.raw_before_defrag=1
-
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_BASE := 0x00000000
