de4408f49ebc8c43df576fd57951c3371e04e1b8
Alexander Koskovich <akoskovich@pm.me>
2022-05-14 16:02:10 -0700

    Spacewar: bluetooth: Drop MAX_L2CAP_CHANNELS
     * This is the default in AOSP.
    
    Change-Id: Ifbfd7deba8d10e4f0a9747e8e170073a0db0c99d
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -23,7 +23,6 @@
 
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
-#define MAX_L2CAP_CHANNELS    32
 #define BLE_VND_INCLUDED   TRUE
 #define GATT_MAX_PHY_CHANNEL  10
 #define AVDT_NUM_SEPS 35
