YukoStudioROM/android_device_nothing_Spacewar

Spacewar: Introduce NothingParts for Glyph Interface
 * Kang FileUtils from LineageOS/android_lineage-sdk@e530d7a.

Co-authored-by: cjybyjk <cjybyjk@zjnu.edu.cn>
Signed-off-by: Kujou Yuko <i@779.moe>
Change-Id: I5a525aadc1d55a86590ecb07ce172667e5d68d25

parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -1,0 +1,21 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.settings;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import org.lineageos.settings.glyph.GlyphUtils;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        GlyphUtils.startService(context);
+    }
+}

parts/src/org/lineageos/settings/glyph/GlyphSettingsActivity.java
@@ -1,0 +1,24 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.settings.glyph;
+
+import android.os.Bundle;
+
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.R;
+
+public class GlyphSettingsActivity extends CollapsingToolbarBaseActivity {
+
+    private static final String TAG_GLYPH = "glyph";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(R.id.content_frame,
+                new GlyphSettingsFragment(), TAG_GLYPH).commit();
+    }
+}

parts/src/org/lineageos/settings/glyph/GlyphSettingsFragment.java
@@ -1,0 +1,67 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.settings.glyph;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Switch;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.SeekBarPreference;
+import androidx.preference.SwitchPreference;
+
+import com.android.settingslib.widget.MainSwitchPreference;
+import com.android.settingslib.widget.OnMainSwitchChangeListener;
+
+import org.lineageos.settings.R;
+
+public class GlyphSettingsFragment extends PreferenceFragment implements
+        Preference.OnPreferenceChangeListener, OnMainSwitchChangeListener {
+
+    private MainSwitchPreference mSwitchBar;
+    private SeekBarPreference mBrightnessPreference;
+    private SwitchPreference mGlyphChargingMeterPreference;
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.glyph_settings);
+
+        boolean isGlyphEnabled = GlyphUtils.isGlyphEnabled(getActivity());
+
+        mSwitchBar = findPreference("glyph_enable");
+        mSwitchBar.setChecked(isGlyphEnabled);
+        mSwitchBar.addOnSwitchChangeListener(this);
+
+        mBrightnessPreference = findPreference("glyph_brightness");
+        mBrightnessPreference.setOnPreferenceChangeListener(this);
+
+        mGlyphChargingMeterPreference = findPreference("glyph_charging_meter");
+        mGlyphChargingMeterPreference.setOnPreferenceChangeListener(this);
+
+        updatePreferences();
+    }
+
+    private void updatePreferences() {
+        boolean isGlyphEnabled = GlyphUtils.isGlyphEnabled(getActivity());
+        mBrightnessPreference.setEnabled(isGlyphEnabled);
+        mGlyphChargingMeterPreference.setEnabled(isGlyphEnabled);
+    }
+
+    @Override
+    public void onSwitchChanged(Switch switchView, boolean isChecked) {
+        GlyphUtils.enableGlyph(getActivity(), isChecked);
+        updatePreferences();
+        GlyphUtils.startService(getActivity());
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        GlyphUtils.startService(getActivity());
+        return true;
+    }
+}

parts/src/org/lineageos/settings/glyph/GlyphUtils.java
@@ -1,0 +1,46 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.settings.glyph;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import androidx.preference.PreferenceManager;
+
+import org.lineageos.settings.util.FileUtils;
+
+public class GlyphUtils {
+
+    public static void startService(Context context) {
+        context.startServiceAsUser(new Intent(context, GlyphService.class), UserHandle.CURRENT);
+    }
+
+    public static void enableGlyph(Context context, boolean enable) {
+        Settings.Secure.putInt(context.getContentResolver(), "glyph_enable", enable ? 1 : 0);
+    }
+
+    public static boolean isGlyphEnabled(Context context) {
+        return Settings.Secure.getInt(context.getContentResolver(), "glyph_enable", 1) != 0;
+    }
+
+    public static boolean isGlyphChargingMeterEnabled(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context).getBoolean(
+                "glyph_charging_meter", false);
+    }
+
+    public static int getGlyphBrightness(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context).getInt(
+                "glyph_brightness", /* default brightness */ 1365);
+    }
+
+    public static void writeLedById(int id, int brightness) {
+        FileUtils.writeLine("/sys/class/leds/aw210xx_led/single_led_br",
+                id + " " + brightness);
+    }
+}

parts/src/org/lineageos/settings/util/FileUtils.java
@@ -1,0 +1,160 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.settings.util;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public final class FileUtils {
+    private static final String TAG = "FileUtils";
+
+    private FileUtils() {
+        // This class is not supposed to be instantiated
+    }
+
+    /**
+     * Reads the first line of text from the given file.
+     * Reference {@link BufferedReader#readLine()} for clarification on what a line is
+     *
+     * @return the read line contents, or null on failure
+     */
+    public static String readOneLine(String fileName) {
+        String line = null;
+        BufferedReader reader = null;
+
+        try {
+            reader = new BufferedReader(new FileReader(fileName), 512);
+            line = reader.readLine();
+        } catch (FileNotFoundException e) {
+            Log.w(TAG, "No such file " + fileName + " for reading", e);
+        } catch (IOException e) {
+            Log.e(TAG, "Could not read from file " + fileName, e);
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                // Ignored, not much we can do anyway
+            }
+        }
+
+        return line;
+    }
+
+    /**
+     * Writes the given value into the given file
+     *
+     * @return true on success, false on failure
+     */
+    public static boolean writeLine(String fileName, String value) {
+        BufferedWriter writer = null;
+
+        try {
+            writer = new BufferedWriter(new FileWriter(fileName));
+            writer.write(value);
+        } catch (FileNotFoundException e) {
+            Log.w(TAG, "No such file " + fileName + " for writing", e);
+            return false;
+        } catch (IOException e) {
+            Log.e(TAG, "Could not write to file " + fileName, e);
+            return false;
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch (IOException e) {
+                // Ignored, not much we can do anyway
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Checks whether the given file exists
+     *
+     * @return true if exists, false if not
+     */
+    public static boolean fileExists(String fileName) {
+        final File file = new File(fileName);
+        return file.exists();
+    }
+
+    /**
+     * Checks whether the given file is readable
+     *
+     * @return true if readable, false if not
+     */
+    public static boolean isFileReadable(String fileName) {
+        final File file = new File(fileName);
+        return file.exists() && file.canRead();
+    }
+
+    /**
+     * Checks whether the given file is writable
+     *
+     * @return true if writable, false if not
+     */
+    public static boolean isFileWritable(String fileName) {
+        final File file = new File(fileName);
+        return file.exists() && file.canWrite();
+    }
+
+    /**
+     * Deletes an existing file
+     *
+     * @return true if the delete was successful, false if not
+     */
+    public static boolean delete(String fileName) {
+        final File file = new File(fileName);
+        boolean ok = false;
+        try {
+            ok = file.delete();
+        } catch (SecurityException e) {
+            Log.w(TAG, "SecurityException trying to delete " + fileName, e);
+        }
+        return ok;
+    }
+
+    /**
+     * Renames an existing file
+     *
+     * @return true if the rename was successful, false if not
+     */
+    public static boolean rename(String srcPath, String dstPath) {
+        final File srcFile = new File(srcPath);
+        final File dstFile = new File(dstPath);
+        boolean ok = false;
+        try {
+            ok = srcFile.renameTo(dstFile);
+        } catch (SecurityException e) {
+            Log.w(TAG, "SecurityException trying to rename " + srcPath + " to " + dstPath, e);
+        } catch (NullPointerException e) {
+            Log.e(TAG, "NullPointerException trying to rename " + srcPath + " to " + dstPath, e);
+        }
+        return ok;
+    }
+}

PixysOS-Devices/device_nothing_Spacewar

parts/src/com/pixys/settings/BootCompletedReceiver.java
@@ -1,0 +1,21 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.pixys.settings;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.pixys.settings.glyph.GlyphUtils;
+
+public class BootCompletedReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        GlyphUtils.startService(context);
+    }
+}

parts/src/com/pixys/settings/glyph/GlyphSettingsActivity.java
@@ -1,0 +1,24 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.pixys.settings.glyph;
+
+import android.os.Bundle;
+
+import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
+import com.android.settingslib.R;
+
+public class GlyphSettingsActivity extends CollapsingToolbarBaseActivity {
+
+    private static final String TAG_GLYPH = "glyph";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(R.id.content_frame,
+                new GlyphSettingsFragment(), TAG_GLYPH).commit();
+    }
+}

parts/src/com/pixys/settings/glyph/GlyphSettingsFragment.java
@@ -1,0 +1,67 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.pixys.settings.glyph;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Switch;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.SeekBarPreference;
+import androidx.preference.SwitchPreference;
+
+import com.android.settingslib.widget.MainSwitchPreference;
+import com.android.settingslib.widget.OnMainSwitchChangeListener;
+
+import com.pixys.settings.R;
+
+public class GlyphSettingsFragment extends PreferenceFragment implements
+        Preference.OnPreferenceChangeListener, OnMainSwitchChangeListener {
+
+    private MainSwitchPreference mSwitchBar;
+    private SeekBarPreference mBrightnessPreference;
+    private SwitchPreference mGlyphChargingMeterPreference;
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.glyph_settings);
+
+        boolean isGlyphEnabled = GlyphUtils.isGlyphEnabled(getActivity());
+
+        mSwitchBar = findPreference("glyph_enable");
+        mSwitchBar.setChecked(isGlyphEnabled);
+        mSwitchBar.addOnSwitchChangeListener(this);
+
+        mBrightnessPreference = findPreference("glyph_brightness");
+        mBrightnessPreference.setOnPreferenceChangeListener(this);
+
+        mGlyphChargingMeterPreference = findPreference("glyph_charging_meter");
+        mGlyphChargingMeterPreference.setOnPreferenceChangeListener(this);
+
+        updatePreferences();
+    }
+
+    private void updatePreferences() {
+        boolean isGlyphEnabled = GlyphUtils.isGlyphEnabled(getActivity());
+        mBrightnessPreference.setEnabled(isGlyphEnabled);
+        mGlyphChargingMeterPreference.setEnabled(isGlyphEnabled);
+    }
+
+    @Override
+    public void onSwitchChanged(Switch switchView, boolean isChecked) {
+        GlyphUtils.enableGlyph(getActivity(), isChecked);
+        updatePreferences();
+        GlyphUtils.startService(getActivity());
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        GlyphUtils.startService(getActivity());
+        return true;
+    }
+}

parts/src/com/pixys/settings/glyph/GlyphUtils.java
@@ -1,0 +1,46 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.pixys.settings.glyph;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+import androidx.preference.PreferenceManager;
+
+import com.pixys.settings.util.FileUtils;
+
+public class GlyphUtils {
+
+    public static void startService(Context context) {
+        context.startServiceAsUser(new Intent(context, GlyphService.class), UserHandle.CURRENT);
+    }
+
+    public static void enableGlyph(Context context, boolean enable) {
+        Settings.Secure.putInt(context.getContentResolver(), "glyph_enable", enable ? 1 : 0);
+    }
+
+    public static boolean isGlyphEnabled(Context context) {
+        return Settings.Secure.getInt(context.getContentResolver(), "glyph_enable", 1) != 0;
+    }
+
+    public static boolean isGlyphChargingMeterEnabled(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context).getBoolean(
+                "glyph_charging_meter", false);
+    }
+
+    public static int getGlyphBrightness(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context).getInt(
+                "glyph_brightness", /* default brightness */ 1365);
+    }
+
+    public static void writeLedById(int id, int brightness) {
+        FileUtils.writeLine("/sys/class/leds/aw210xx_led/single_led_br",
+                id + " " + brightness);
+    }
+}

parts/src/com/pixys/settings/util/FileUtils.java
@@ -1,0 +1,160 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.pixys.settings.util;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public final class FileUtils {
+    private static final String TAG = "FileUtils";
+
+    private FileUtils() {
+        // This class is not supposed to be instantiated
+    }
+
+    /**
+     * Reads the first line of text from the given file.
+     * Reference {@link BufferedReader#readLine()} for clarification on what a line is
+     *
+     * @return the read line contents, or null on failure
+     */
+    public static String readOneLine(String fileName) {
+        String line = null;
+        BufferedReader reader = null;
+
+        try {
+            reader = new BufferedReader(new FileReader(fileName), 512);
+            line = reader.readLine();
+        } catch (FileNotFoundException e) {
+            Log.w(TAG, "No such file " + fileName + " for reading", e);
+        } catch (IOException e) {
+            Log.e(TAG, "Could not read from file " + fileName, e);
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                // Ignored, not much we can do anyway
+            }
+        }
+
+        return line;
+    }
+
+    /**
+     * Writes the given value into the given file
+     *
+     * @return true on success, false on failure
+     */
+    public static boolean writeLine(String fileName, String value) {
+        BufferedWriter writer = null;
+
+        try {
+            writer = new BufferedWriter(new FileWriter(fileName));
+            writer.write(value);
+        } catch (FileNotFoundException e) {
+            Log.w(TAG, "No such file " + fileName + " for writing", e);
+            return false;
+        } catch (IOException e) {
+            Log.e(TAG, "Could not write to file " + fileName, e);
+            return false;
+        } finally {
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch (IOException e) {
+                // Ignored, not much we can do anyway
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Checks whether the given file exists
+     *
+     * @return true if exists, false if not
+     */
+    public static boolean fileExists(String fileName) {
+        final File file = new File(fileName);
+        return file.exists();
+    }
+
+    /**
+     * Checks whether the given file is readable
+     *
+     * @return true if readable, false if not
+     */
+    public static boolean isFileReadable(String fileName) {
+        final File file = new File(fileName);
+        return file.exists() && file.canRead();
+    }
+
+    /**
+     * Checks whether the given file is writable
+     *
+     * @return true if writable, false if not
+     */
+    public static boolean isFileWritable(String fileName) {
+        final File file = new File(fileName);
+        return file.exists() && file.canWrite();
+    }
+
+    /**
+     * Deletes an existing file
+     *
+     * @return true if the delete was successful, false if not
+     */
+    public static boolean delete(String fileName) {
+        final File file = new File(fileName);
+        boolean ok = false;
+        try {
+            ok = file.delete();
+        } catch (SecurityException e) {
+            Log.w(TAG, "SecurityException trying to delete " + fileName, e);
+        }
+        return ok;
+    }
+
+    /**
+     * Renames an existing file
+     *
+     * @return true if the rename was successful, false if not
+     */
+    public static boolean rename(String srcPath, String dstPath) {
+        final File srcFile = new File(srcPath);
+        final File dstFile = new File(dstPath);
+        boolean ok = false;
+        try {
+            ok = srcFile.renameTo(dstFile);
+        } catch (SecurityException e) {
+            Log.w(TAG, "SecurityException trying to rename " + srcPath + " to " + dstPath, e);
+        } catch (NullPointerException e) {
+            Log.e(TAG, "NullPointerException trying to rename " + srcPath + " to " + dstPath, e);
+        }
+        return ok;
+    }
+}