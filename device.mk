93299bb0671879d340ca03fde418ad4b09da601b
aswin7469 <aswinas@pixysos.com>
2022-11-02 19:30:04 +0530

    Spacewar: Add Nothing Camera
    * huge thanks to aospa for porting Ntexperience dependencies
    
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- device.mk
+++ device.mk
@@ -10,6 +10,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/nothing/Spacewar/Spacewar-vendor.mk)
 
+# NT Camera
+$(call inherit-product, vendor/nothing/camera/nothing-camera.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
