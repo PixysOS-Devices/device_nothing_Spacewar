91990fb9ab07557891341f16a53202c09848511b
aswin7469 <aswinas@pixysos.com>
2022-12-13 16:31:36 +0530

    Spacewar: import msm_irqbalance.conf
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- device.mk
+++ device.mk
@@ -355,6 +355,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.2.vendor
 
+# IRQ balance config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
