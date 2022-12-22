9a3f0a8e6414a46b3429fada15031ae49c8136e6
aswin7469 <aswinas@pixysos.com>
2022-12-23 03:28:58 +0530

    Revert "Revert "Spacewar: Switch BtAudio to AIDL""
    * seems like its fixed on QPR1
    
    This reverts commit 16b562dd30f109ea438f67801ea82af9b6fd6d2f.

--- device.mk
+++ device.mk
@@ -117,7 +117,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio@2.0-impl \
     android.hardware.soundtrigger@2.3-impl \
     audio.bluetooth.default \
     audio.r_submix.default \
@@ -190,7 +189,7 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
-    android.hardware.bluetooth.audio@2.1-impl \
+    android.hardware.bluetooth.audio-impl \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
